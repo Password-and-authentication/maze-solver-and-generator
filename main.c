8ed98149267c32837e149065e727e52f0023ea08
kyopa <pomelleero@gmail.com>
2022-07-02 12:30:31 +0300

    rename variables / clean up

--- main.c
+++ main.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
+
+#define TOP_ROW 1
+#define BOT_ROW 9
+#define FAR_LEFT 0
+#define FAR_RIGHT 9
+
 typedef struct cellstruct *cellptr;
 
-typedef struct neighbor {
-    cellptr cell;
-    int *wall;
-} neighbor;
 
 typedef struct cellstruct {
-    neighbor *left;
-    neighbor *right;
-    neighbor *top;
-    neighbor *bot;
+    cellptr left;
+    cellptr right;
+    cellptr top;
+    cellptr bot;
+    int *walls[2];              // [0] = top wall, [1] = right wall
 
     int visited;
 } cell;
@@ -26,7 +29,7 @@ int main() {
     initializeMaze(*maze);
     cellptr c = &maze[9][9];
     c->visited = 1;
-    c->left->cell->visited = 1;
+    c->left->visited = 1;
     printMaze(*maze);
 
     
@@ -41,35 +44,38 @@ void printMaze(cellptr maze) {
     for (int i = 0; i< 100; ++i) {
         //if (cptr->right == NULL) printf("NEW ROW\n");
         printf("%d ", cptr->visited);
-        if (*cptr->right->wall == 1) {
-
-        }
-        if (cptr++->right->cell == NULL) putchar(10);
+        
+        if (cptr++->right == NULL) putchar(10);
     }
 }
 
 void initializeMaze(cellptr maze) {
-    cell *c, *d;
-    int i, j;
+    cellptr rowptr, colptr;
+    int row, col;
 
-    for (i = 0; i < 10; ++i) {
-        c = &maze[10 * i];
-        d = c;
-        for (j = 0; j < 10; ++j) {  
-            if (i == 0) {
-                d->top->cell = NULL;
-            } else d->top->cell = &(c - 10)[j];
-            if (i == 9) {
-                d->bot->cell = NULL;
-            } else d->bot->cell = &(c + 10)[j];
-            d->visited = 0;
-            if (j == 0) {
-                d->left->cell = NULL;
-            } else d->left->cell = (d - 1);
-            if (j == 9) {
-                d->right->cell = NULL;
-            } else d->right->cell = (d + 1);
-            ++d;     
+    for (row = 0; row < 10; ++row) {
+        rowptr = &maze[10 * row];
+        colptr = rowptr;
+        for (col = 0; col < 10; ++col) {  
+            if (row == TOP_ROW) {
+                colptr->top = NULL;
+            } else {
+                colptr->top = &(rowptr - 10)[col];
+                
+
+            }
+            if (row == BOT_ROW) {
+                colptr->bot = NULL;
+            } else colptr->bot = &(rowptr + 10)[col];
+            if (col == FAR_LEFT) {
+                colptr->left = NULL;
+            } else colptr->left = (colptr - 1);
+            if (col == FAR_RIGHT) {
+                colptr->right = NULL;
+            } else colptr->right = (colptr + 1);
+
+            colptr->visited = 0;
+            ++colptr;     
         }
         
     } 
