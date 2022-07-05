3b2e57ec47cbe5f3f0a518d7e9dcfa50cdd08eb9
kyopa <pomelleero@gmail.com>
2022-07-05 12:05:07 +0300

    getpoint

--- main.c
+++ main.c
@@ -27,11 +27,41 @@ int main(int argc, char *argv[]) {
         cellptr++->visited = false;    
     }
 
-    point s = {0, 0};               // custom starting / end point not yet implemented
-    point e = {6, 6};
-    solve(&maze, s, e);
+    point getpoint();
+    point s, e;
+    while (true) {
+        printf("Point format: x,x\n");
+        printf("Enter starting point: ");
+        s = getpoint();
+        printf("Enter ending point: ");
+        e = getpoint();
+        if (s.x >= colMAX || s.y >= rowMAX || e.x >= colMAX || e.y >= rowMAX || s.x < 0 || s.y < 0 || e.x < 0 || e.y < 0) {
+            printf("\nCoordinate to high or low\n\n\n");
+            continue;
+        }
+        break; 
+    }
+    //solve(&maze, s, e);
 
-    printmaze(&maze);
+    //printmaze(&maze);
+    //putchar('\n');
     return 0;
 }
+
+point getpoint() {
+    char str[10];
+    char sx[10];
+    char sy[10];
+
+    scanf("%s", str);
+    int i, j;
+    i = j = 0;
+    while ((sx[i] = str[i]) != ',' && ++i);
+    sx[i] = '\n';
+    ++i;
+    while ((sy[j] = str[i]) != '\n' && ++i && ++j);
+    sy[j] = '\n';
+    point p = {atoi(sx), atoi(sy)};
+    return p;
+}   
 
